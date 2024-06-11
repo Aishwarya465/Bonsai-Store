faddccdde962c89f42b2463c0677a35281afced5
Aishwarya S <92157139+Aishwarya465@users.noreply.github.com>
2024-06-11 15:11:52 +0530

    Add files via upload

---
+++ database/shopping_db.sql
@@ -0,0 +1,297 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `com_logo` varchar(100) DEFAULT NULL,
+  `com_name` varchar(100) NOT NULL,
+  `com_email` varchar(60) NOT NULL,
+  `com_phone` varchar(15) DEFAULT NULL,
+  `com_address` varchar(255) DEFAULT NULL,
+  `cur_format` varchar(10) NOT NULL,
+  `admin_role` tinyint(4) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_name`, `username`, `password`, `com_logo`, `com_name`, `com_email`, `com_phone`, `com_address`, `cur_format`, `admin_role`) VALUES
+(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL, 'Inventory', 'inventory@gmail.com', NULL, NULL, 'Rs.', 1);
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_title` text NOT NULL,
+  `brand_cat` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(100) NOT NULL,
+  `cat_title` text NOT NULL,
+  `products` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `s_no` int(11) NOT NULL,
+  `site_name` varchar(100) NOT NULL,
+  `site_title` varchar(100) DEFAULT NULL,
+  `site_logo` varchar(100) NOT NULL,
+  `site_desc` varchar(255) DEFAULT NULL,
+  `footer_text` varchar(100) NOT NULL,
+  `currency_format` varchar(20) NOT NULL,
+  `contact_phone` varchar(15) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `contact_address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`s_no`, `site_name`, `site_title`, `site_logo`, `site_desc`, `footer_text`, `currency_format`, `contact_phone`, `contact_email`, `contact_address`) VALUES
+(1, 'Bonsai', 'Shop for your favorite plant, gardening products, seeds and many more...', '1660022454bonsai-logo.png', 'A plant needs sunlight, carbon dioxide, minerals and water to make food by photosynthesis. A green substance in plants called chlorophyll traps the energy from the Sun needed to make food.', 'Copyright 2022', 'Rs.', '8618956911', 'nisarga@email1.com', '#123, India');
+
+
+--
+-- Table structure for table `order_products`
+--
+
+CREATE TABLE `order_products` (
+  `order_id` int(11) NOT NULL,
+  `product_id` varchar(100) NOT NULL,
+  `product_qty` varchar(100) NOT NULL,
+  `total_amount` varchar(10) NOT NULL,
+  `product_user` int(11) NOT NULL,
+  `order_date` varchar(11) NOT NULL,
+  `pay_req_id` varchar(100) DEFAULT NULL,
+  `confirm` tinyint(4) NOT NULL DEFAULT 0,
+  `delivery` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int(11) NOT NULL,
+  `item_number` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `txn_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `payment_gross` float(10,2) NOT NULL,
+  `currency_code` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `payment_status` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(100) NOT NULL,
+  `product_code` varchar(100) NOT NULL,
+  `product_cat` int(100) NOT NULL,
+  `product_sub_cat` int(11) NOT NULL,
+  `product_brand` int(100) DEFAULT NULL,
+  `product_title` varchar(255) NOT NULL,
+  `product_price` varchar(100) NOT NULL,
+  `product_desc` text NOT NULL,
+  `featured_image` text NOT NULL,
+  `qty` int(11) NOT NULL DEFAULT 1,
+  `product_keywords` text DEFAULT NULL,
+  `product_views` int(11) DEFAULT 0,
+  `product_status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `product_cart`
+--
+
+CREATE TABLE `product_cart` (
+  `s_no` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `sub_categories`
+--
+
+CREATE TABLE `sub_categories` (
+  `sub_cat_id` int(11) NOT NULL,
+  `sub_cat_title` varchar(100) NOT NULL,
+  `cat_parent` int(11) NOT NULL,
+  `cat_products` int(11) NOT NULL DEFAULT 0,
+  `show_in_header` tinyint(4) NOT NULL DEFAULT 1,
+  `show_in_footer` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `f_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `address` text NOT NULL,
+  `city` text NOT NULL,
+  `user_role` int(11) DEFAULT 1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `order_products`
+--
+ALTER TABLE `order_products`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `product_cart`
+--
+ALTER TABLE `product_cart`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD PRIMARY KEY (`sub_cat_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `options`
+--
+ALTER TABLE `options`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_products`
+--
+ALTER TABLE `order_products`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `product_cart`
+--
+ALTER TABLE `product_cart`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  MODIFY `sub_cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
